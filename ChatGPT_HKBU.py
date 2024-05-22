e7102af51ba1b5c1f9cddddfb25f297849f50794
COMP7940 <s19412541@life.hkbu.edu.hk>
2024-05-22 16:23:58 +0800

    finished Lab8

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -1,20 +1,31 @@
+# ChatGPT_HKBU.py
 import configparser
 import requests
+import os
 
 class HKBU_ChatGPT():
-
-    def __init__(self, config_path = './config.ini'):
+    def __init__(self,config_= './config.ini'):
+        pass
         # if type(config_path) == str:
-        self.config = configparser.ConfigParser()
-        self.config.read(config_path)
+        # Lab8P2Step1 as below:
+        # -----------------------------
+        # self.config = configparser.ConfigParser()
+        # self.config.read(config_path)
+        # -----------------------------
+        # Lab8P2Step1 as above:
         # elif type(self.config) == configparser.ConfigParser:
         #     self.config = config_path
 
     def submit(self,message):
-        print(self.config['CHATGPT']['MODELNAME'])
+        # print(self.config['CHATGPT']['MODELNAME'])
         conversation = [{"role": "user", "content": message}]
-        url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
-        headers = { 'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+        # modifying following code from Lab8 Recording P2Step1
+        url = ('https://chatgpt.hkbu.edu.hk/general/rest') + "/deployments/" + ('gpt-35-turbo') + "/chat/completions/?api-version=" + ('2024-02-15-preview')
+        # modifying following code correspondingly
+        headers = { 'Content-Type': 'application/json', 'api-key': (os.environ['ACCESS_TOKEN_CHATGPT']) }
+        #The api-key reference to line 8 in part2.yaml
+        #If your want to run in local machine, Plz run 'export ACCESS_TOKEN_CHATGPT=c813af90-2980-4c99-9bf5-8cf8e0eaa493' to define this in terminal.
+        
         payload = { 'messages': conversation }
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
@@ -23,6 +34,7 @@ class HKBU_ChatGPT():
         else:
             print(response.text)
             return 'Error:', response
+            
 if __name__ == '__main__':
     ChatGPT_test = HKBU_ChatGPT()
     while True:
--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #chatbot.py
+import os #Lab5.7.1
+import json
 import telegram
 from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
-import os #Lab5.7.1
-import configparser #Lab5.7.2
+# import configparser #Lab5.7.2 and Lab8P2
+#import configparser Lab8P2Step1
 import logging
 import redis
+import requests
+
 
 from ChatGPT_HKBU import HKBU_ChatGPT
 def equiped_chatgpt(update, context):
@@ -18,15 +22,19 @@ def equiped_chatgpt(update, context):
 #global redis1
 def main():
     #Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    #lab8P2 as below
+    #config = configparser.ConfigParser() Lab8P2Step1
+    # config.read('config.ini') Lab8P2Step1
+    #Lab8.P2 as below:
+    updater = Updater(token=(os.environ['ACCESS_TOKEN_TG']), use_context=True)
+    # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     # updater = Updater(token=('6565812077:AAEl-75tFOKjtPYhglY_xXCx7dswWEvsTxo'), use_context=True) #Lab5.7.
     dispatcher = updater.dispatcher
+    
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']),
-    password=(config['REDIS']['PASSWORD']),
-    port=(config['REDIS']['REDISPORT']))
+    # redis1 = redis.Redis(host=(config['REDIS']['HOST']),password=(config['REDIS']['PASSWORD']),port=(config['REDIS']['REDISPORT']))
+    #lab8P2 as below
+    redis1 = redis.Redis(os.environ['HOST_REDIS'], password=(os.environ['PASSWORD_REDIS']), port=(os.environ['PORT_REDIS']))  
 
     #you can set this logging module, so you will know when
     #and why things do not as expected Meanwhile, update your config.ini as:
@@ -76,7 +84,23 @@ def add(update: Update, context: CallbackContext) -> None:
         update.message.reply_text('You have said ' + msg +  ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /add <keyword>')
-
+        
+# See following code in 'ChatGPT_HKBU.py'
+#----------------------------------------
+# def submit(self, message):
+#     conversation = [{"role": "user", "content": message}]
+#     # url = (os.environ['BASICURL']) + "/deployments/" + (os.environ['MODELNAME']) + "/chat/completion/?api-version=" + (os.environ{APIVERSION})
+#     headers = {'Content-Type': 'application/json','api-key': (os.environ['ACCESS_TOKEN_CHATGPT'])}
+#     url = 
+#     payload = {'messages': conversation }
+#     response = requests.post(url,json=payload, headers=headers)
+#     if response.status_code == 200;
+#         data = response.json()
+#         return data['choices'][0]['message']['content']
+#     else:
+#         return 'Error:', response
+#----------------------------------------    
+    
 if __name__ == '__main__':
     main()
     #print('test')Print terminal log
